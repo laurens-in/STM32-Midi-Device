0b4e7d884597981be72644d4f67373830b1914ce
laurens <laurens.inauen@hkb.bfh.ch>
2024-01-09 20:19:45 +0100

    remove wrongly tracked files

--- Debug/tinyusb/src/class/dfu/dfu_device.d
+++
@@ -1,9 +0,0 @@
-tinyusb/src/class/dfu/dfu_device.o: ../tinyusb/src/class/dfu/dfu_device.c \
- /home/laurens/STM32CubeIDE/workspace_1.13.2/STM-Midi-Firmware/tinyusb/src/tusb_option.h \
- /home/laurens/STM32CubeIDE/workspace_1.13.2/STM-Midi-Firmware/tinyusb/src/common/tusb_compiler.h \
- ../Core/Inc/tusb_config.h \
- /home/laurens/STM32CubeIDE/workspace_1.13.2/STM-Midi-Firmware/tinyusb/src/common/tusb_mcu.h
-/home/laurens/STM32CubeIDE/workspace_1.13.2/STM-Midi-Firmware/tinyusb/src/tusb_option.h:
-/home/laurens/STM32CubeIDE/workspace_1.13.2/STM-Midi-Firmware/tinyusb/src/common/tusb_compiler.h:
-../Core/Inc/tusb_config.h:
-/home/laurens/STM32CubeIDE/workspace_1.13.2/STM-Midi-Firmware/tinyusb/src/common/tusb_mcu.h:
